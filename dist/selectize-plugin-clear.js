b410563d01f732e7b9041cb95f44de1a9058f375
Patryk Wasiewicz <patryk.wasiewicz@outlook.com>
2016-11-11 10:06:19 +0100

    Hiding button when no value, closing select after clearing (optional)

--- dist/selectize-plugin-clear.js
+++ dist/selectize-plugin-clear.js
@@ -1,11 +1,11 @@
-Selectize.define('clear_button', function(options) {
+Selectize.define('clear_button', function (options) {
     /**
      * Escapes a string for use within HTML.
      *
      * @param {string} str
      * @returns {string}
      */
-    var escape_html = function(str) {
+    var escape_html = function (str) {
         return (str + '')
             .replace(/&/g, '&amp;')
             .replace(/</g, '&lt;')
@@ -14,34 +14,59 @@ Selectize.define('clear_button', function(options) {
     };
 
     options = $.extend({
-        label     : '&times;',
-        title     : 'Remove',
-        className : 'clearAll',
-        append    : true
+        label: '&times;',
+        title: 'Remove',
+        className: 'clearAll',
+        append: true,
+        hideWhenEmpty: true,
+        leaveOpen: false
     }, options);
 
     var self = this,
-        html = '<span class="' + options.className + '" tabindex="-1" title="' + escape_html(options.title) + '">' + options.label + '</span>';
+        $html = $('<span class="' +
+            options.className +
+            '" tabindex="-1" title="' +
+            escape_html(options.title) +
+            '">' +
+            options.label +
+            '</span>');
 
 
-    this.setup = (function() {
+    this.setup = (function () {
         var original = self.setup;
-        return function() {
+        return function () {
             // override the item rendering method to add the button to each
             original.apply(this, arguments);
 
-            this.$wrapper.append(html);
+            this.$wrapper.append($html);
+
+            if (options.hideWhenEmpty) {
+                var $input = this.$input;
+                var hideShowClrBtn = function ($inpt) {
+                    var val = $inpt.val();
+                    if (val) {
+                        $html.show();
+                    } else {
+                        $html.hide();
+                    }
+                }
+
+                hideShowClrBtn($input);
+                $input.change(function () {
+                    hideShowClrBtn($input);
+                });
+            }
 
             // add event listener
-            this.$wrapper.on('click', '.' + options.className, function(e) {
+            this.$wrapper.on('click', '.' + options.className, function (e) {
                 e.preventDefault();
                 if (self.isLocked) return;
                 self.clear();
-                self.clearOptions();
-                self.$control_input.focus();
-            });
 
+                if (options.leaveOpen) {
+                    self.$control_input.focus();
+                }
+            });
         };
     })();
-
 });
